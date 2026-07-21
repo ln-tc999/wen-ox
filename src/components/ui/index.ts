ea34a7743cd8e61e45fa4fbbc8cf0ac433e56ae3
maulana-tech <firdaussyah03@gmail.com>
2026-07-21 19:05:57 +0700

    refactor(src/components/ui/index.ts): auto update

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,5 @@
 export { type FeatureCard, FeatureSections } from "./feature-sections";
+export { FluidVolume } from "./fluid-volume";
 export { Navbar1 } from "./navbar-1";
+export { ScrambleCycle } from "./scramble-cycle";
 export { WalletButton } from "./wallet-button";
