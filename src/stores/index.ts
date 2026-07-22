8c0f99419adb2659d58159f626cdd50eda5a0b67
maulana-tech <firdaussyah03@gmail.com>
2026-07-22 18:34:55 +0700

    refactor(src/stores/index.ts): auto update

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -7,9 +7,7 @@ export {
 export type { DepositStep as LifiDepositStep } from "./deposit-store";
 export * from "./expert-store";
 export * from "./meta-store";
-export {
-  useNoxDepositStore,
-} from "./nox-deposit-store";
+export { useNoxDepositStore } from "./nox-deposit-store";
 export type { DepositStep as NoxDepositStep } from "./nox-deposit-store";
 export * from "./nox-withdraw-store";
 export * from "./portfolio-store";
