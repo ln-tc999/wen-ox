48ed36a4633066538e71e0092b30e13adc5a63b7
maulana-tech <firdaussyah03@gmail.com>
2026-07-22 18:27:29 +0700

    refactor(src/stores/index.ts): feat(deposit): add deposit/withdraw page with Nox Protocol integration

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,16 @@
 export * from "./compare-store";
-export * from "./deposit-store";
+export {
+  useDepositStore,
+  ERC4626_DIRECT_DEPOSIT_TOOL,
+  ERC4626_WRAP_AND_DEPOSIT_TOOL,
+} from "./deposit-store";
+export type { DepositStep as LifiDepositStep } from "./deposit-store";
 export * from "./expert-store";
 export * from "./meta-store";
-export * from "./nox-deposit-store";
+export {
+  useNoxDepositStore,
+} from "./nox-deposit-store";
+export type { DepositStep as NoxDepositStep } from "./nox-deposit-store";
 export * from "./nox-withdraw-store";
 export * from "./portfolio-store";
 export * from "./swap-store";
