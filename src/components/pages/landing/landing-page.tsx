5a55815a9d74795f73db0e110fd59cd6969c1a4d
maulana-tech <firdaussyah03@gmail.com>
2026-07-30 18:27:26 +0700

    refactor(src/components/pages/landing/landing-page.tsx): auto update

--- src/components/pages/landing/landing-page.tsx
+++ src/components/pages/landing/landing-page.tsx
@@ -2,6 +2,7 @@ import { FeaturesSection } from "./features-section";
 import { FooterSection } from "./footer-section";
 import { HeroSection } from "./hero-section";
 import { LandingNavbar } from "./landing-navbar";
+import { PublicLedgerSection } from "./public-ledger-section";
 
 export function LandingPage() {
   return (
@@ -9,6 +10,7 @@ export function LandingPage() {
       <LandingNavbar />
       <main className="grow">
         <HeroSection />
+        <PublicLedgerSection />
         <FeaturesSection />
       </main>
       <FooterSection />
