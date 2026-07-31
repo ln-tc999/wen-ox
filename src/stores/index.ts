61e2686c5cf6067c51d62cb4297fbad4003642b7
maulana-tech <firdaussyah03@gmail.com>
2026-07-31 14:58:20 +0700

    refactor(src/stores/index.ts): auto update

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,17 +1,17 @@
 export * from "./compare-store";
+export type { DepositStep as LifiDepositStep } from "./deposit-store";
 export {
-  useDepositStore,
   ERC4626_DIRECT_DEPOSIT_TOOL,
   ERC4626_WRAP_AND_DEPOSIT_TOOL,
+  useDepositStore,
 } from "./deposit-store";
-export type { DepositStep as LifiDepositStep } from "./deposit-store";
 export * from "./expert-store";
 export * from "./meta-store";
-export { useNoxDepositStore } from "./nox-deposit-store";
 export type { DepositStep as NoxDepositStep } from "./nox-deposit-store";
-export { useNoxWithdrawStore } from "./nox-withdraw-store";
+export { useNoxDepositStore } from "./nox-deposit-store";
 export type { WithdrawStep as NoxWithdrawStep } from "./nox-withdraw-store";
+export { useNoxWithdrawStore } from "./nox-withdraw-store";
 export * from "./portfolio-store";
 export * from "./swap-store";
-export { useWithdrawStore, ERC4626_REDEEM_TOOL } from "./withdraw-store";
 export type { WithdrawStep as LifiWithdrawStep } from "./withdraw-store";
+export { ERC4626_REDEEM_TOOL, useWithdrawStore } from "./withdraw-store";
