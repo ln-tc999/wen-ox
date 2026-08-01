90fa330ffe9cf45b779e43047f509f69cf66311a
maulana-tech <firdaussyah03@gmail.com>
2026-08-01 12:23:36 +0700

    refactor(src/app/(app)/faucet/page.tsx): auto update

--- src/app/(app)/faucet/page.tsx
+++ src/app/(app)/faucet/page.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-import { FaucetView } from "@/components/pages/(app)/faucet/faucet-view";
+import { FaucetViewWrapper } from "@/components/pages/(app)/faucet/faucet-view-wrapper";
+
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: "Wen-Ox | Testnet Faucet Guide",
   description:
-    "Get testnet Arbitrum Sepolia ETH, USDC, and RLC tokens to test Wen-Ox confidential vaults.",
+    "Get testnet Arbitrum Sepolia ETH, USDC, RLC, and WETH tokens to test Wen-Ox confidential vaults.",
 };
 
 export default function FaucetPage() {
-  return <FaucetView />;
+  return <FaucetViewWrapper />;
 }
